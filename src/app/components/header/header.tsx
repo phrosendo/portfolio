517bf381daf1246a4479be3faddeb7f9b0a95c9e
pedrogedo <pedro.ads@outlook.com>
2023-10-11 21:22:42 -0300

    Adicionado estrutura inical hml/ Criação do header

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import "./header.scss"
+
+export function Header(){
+    return(
+        <div className="header">
+        <div>
+          <h1>Olá, eu sou o Pedro!👋</h1>
+          <h2>Software Developer</h2>
+        </div>
+        <Image
+          src="/ph.jpeg"
+          alt="Vercel Logo"
+          width={325}
+          height={310}
+          priority
+        />
+      </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,35 @@
-import Image from 'next/image'
+import { Header } from "./components/header/header";
+import "./styles/home.scss"
 
 export default function Home() {
   return (
-    <main>
-      <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        width={100}
-        height={24}
-        priority
-      />
+    <main className="container">
+      <Header/>
+      <div className="experience">
+        <h3>Experiência</h3>
+        <p>2 anos trabalhando como QA/Developer na empresa Stefanini</p>
+        <div className="experience-time">
+
+        </div>
+        <div className="infos">
+          <h3>Idiomas</h3>
+          <div className="languages-infos">
+            <span>🇧🇷 PT-BR - Fluent</span>
+            <span>🇺🇸 EN - Basic</span>
+          </div>
+          <h3>Educação</h3>
+          <div className="educational-info">
+          <span>🎓</span>
+          <span>Análise e Desenvolvimento de Sistemas - Faculdade Metropolitanas Unidas (FMU)</span>
+          </div>
+          <div className="buttons">
+            <div className="social">
+
+            </div>
+            <button>contato</button>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
